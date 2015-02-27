be989105db709bed23b19d156efb4d9fc90d0285
Franky Lau <franky.lau@oocl.com>
2015-02-27 18:42:13 +0800

    Move the Ticker related logic from game.js to cal.js

--- client/js/game.js
+++ client/js/game.js
@@ -1,26 +1,13 @@
 (function(){
   'use strict';
   var worker = new Worker('js/worker/cal.js'); //HTML5 Web worker
-  var mp=0,c=null;
-  var ENDFLAG = false;
+  var mp=0,tk=0;
+  var STATE_TYPE = {Menu:0, Play:1, End:2};
+  var gameState = STATE_TYPE.Menu;
   var seq; // Need to fetch from server
   var jsondata;
   var postAjaxRequest = azdart.postAjaxRequest;
   var setContentByDocId = azdart.setContentByDocId;
-  var ticker = new TickerSingleton();
-
-  function TickerSingleton(){
-    var tk = 0; // Private member in class
-    this.inc = function(){
-      tk++; // Privileged
-    };
-    this.get = function(){
-      return tk;
-    };
-    this.clear = function(){
-      tk = 0;
-    };
-  }
 
   postAjaxRequest('/getSeq',null,function(res){
     jsondata = JSON.parse(res);
@@ -42,43 +29,35 @@
   },function(res){
   });
 
-  function launchCounter() {
-    worker.onmessage = function(e) {
-      setContentByDocId("count",e.data.value);
-    };
-    worker.onerror = function(e) {
-      console.log('Error',e);
-    };
-    worker.postMessage({'cmd':'cal','value': ticker.get()});
-  }
+  worker.onmessage = function(e) {
+    tk = e.data.tick;
+    setContentByDocId("count",e.data.value);
+  };
+  worker.onerror = function(e) {
+    console.log('Error',e);
+  };
 
   function resetGame(){
-    ticker.clear();
+    worker.postMessage({'cmd':'resetTimer'});
     mp = 0;
-    ENDFLAG = false;
+    gameState = STATE_TYPE.Menu;
     setContentByDocId("count",i18n.title);
     setContentByDocId("inp",seq);
-    clearInterval(c);
-    c = null;
   }
 
   function startGame(){
-    c=setInterval(function(){
-      ticker.inc();
-      launchCounter();
-    },10);
+    gameState = STATE_TYPE.Play;
+    worker.postMessage({'cmd':'startTimer'});
   }
 
   function endGame(){
-    clearInterval(c);
-    ENDFLAG = true;
-    c = null;
+    gameState = STATE_TYPE.End;
     setContentByDocId("inp","<input placeholder=\""+i18n.namePlaceholder+"\" class=\"textfield\" id=\"name\" type=\"text\" />");
   }
 
   function submitResult(){
     // Submit a post to backend
-    postAjaxRequest('/submit',JSON.stringify({name:document.getElementById("name").value,ticker:ticker.get(),sequence:jsondata.seqId}),function(res){
+    postAjaxRequest('/submit',JSON.stringify({name:document.getElementById("name").value,ticker:tk,sequence:jsondata.seqId}),function(res){
       setContentByDocId("inp",jsondata.seq);
       resetGame();
     },function(res){
@@ -89,13 +68,13 @@
   window.onkeydown=function(e){
     // Game Logic
     var inp=String.fromCharCode(e.keyCode?e.keyCode:e.which).toLowerCase();
-    if(ENDFLAG){
-      if(e.keyCode===13) submitResult();
+    if(gameState===STATE_TYPE.End){
+      if(e.keyCode===13) submitResult(); // #ENTER key
     } else {
-      if(e.keyCode===32){
+      if(e.keyCode===32){ // #SPACE key
         resetGame(); //Fast key for restart the game (using #space)
       } else {
-        if(c===null) startGame();
+        if(gameState===STATE_TYPE.Menu) startGame();
         if(seq[mp]===inp){
           setContentByDocId("inp",'<span class=\"finished\">'+seq.substring(0,mp+1)+'</span>'+seq.substring(mp+1));
           if(mp++===seq.length-1) endGame();
--- client/js/worker/cal.js
+++ client/js/worker/cal.js
@@ -1,11 +1,53 @@
+/*
+ * Web Worker Timer
+ */
+var self = this;
+
+(function(){
+var ticker = (function(){ // Ticker Singleton
+  var tk = 0; // Private member
+  return {
+    inc  : function(){ return ++tk; },
+    get  : function(){ return tk; },
+    clear: function(){ tk = 0; }
+  };
+})();
+
+var timer = (function(){
+  var intvFunc;
+  return {
+    start: function (){
+      cal(ticker.get());
+      intvFunc = intvFunc != null ? intvFunc : setInterval(function(){
+        cal(ticker.inc());
+      }, 10);
+    },
+    end: function (){
+      clearInterval(intvFunc);
+      intvFunc = null;
+    },
+    reset: function (){
+      ticker.clear();
+    }
+  };
+})();
+
 function cal(tk){
 
   var ms=tk%100;
   var s=((tk-ms)/100)%60;
   var mn=Math.floor(((tk-ms)/100)/60);
-  self.postMessage({'value':(mn<10?"0"+mn:mn)+":"+(s<10?"0"+s:s)+":"+(ms<10?"0"+ms:ms)});
+  self.postMessage({'tick':ticker.get(),'value':(mn<10?"0"+mn:mn)+":"+(s<10?"0"+s:s)+":"+(ms<10?"0"+ms:ms)});
 }
 
+// export function
+self.startTimer = timer.start;
+self.endTimer   = timer.end;
+self.resetTimer = timer.reset;
+})();
+
 self.onmessage = function(e) {
-  cal(e.data.value);
+  if(e.data && e.data.cmd && typeof self[e.data.cmd] === 'function'){
+    self[e.data.cmd]();
+  }
 };
